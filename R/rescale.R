fde7abcdf4171d2621a90e8ef9738c06666a21c3
Jian Yen <jdl.yen@gmail.com>
2020-05-05 11:31:05 +1000

    fix args in rescale funs

--- R/rescale.R
+++ R/rescale.R
@@ -14,7 +14,8 @@ NULL
 #' @param subset a numeric vector defining years in which the metric should be calculated.
 #'   Can be a continuous or discontinuous set of years
 #' @param season define months of year in which to calculate the metric (see details)
-#' @param \dots additional arguments passed to \code{median} (e.g. `na.rm = TRUE`)
+#' @param \dots additional arguments passed to \code{median}, \code{mean}, \code{max},
+#'   or \code{fun} (e.g. `na.rm = TRUE`)
 #'
 by_median <- function(subset, season = 1:12, ...) {
 
@@ -31,11 +32,6 @@ by_median <- function(subset, season = 1:12, ...) {
 #'
 #' @export
 #'
-#' @param subset a numeric vector defining years in which the metric should be calculated.
-#'   Can be a continuous or discontinuous set of years
-#' @param season define months of year in which to calculate the metric (see details)
-#' @param \dots additional arguments passed to \code{mean} (e.g. `na.rm = TRUE`)
-#'
 by_mean <- function(subset, season = 1:12, ...) {
 
   if (missing(subset))
@@ -50,11 +46,6 @@ by_mean <- function(subset, season = 1:12, ...) {
 #' @rdname rescale
 #'
 #' @export
-#'
-#' @param subset a numeric vector defining years in which the metric should be calculated.
-#'   Can be a continuous or discontinuous set of years
-#' @param season define months of year in which to calculate the metric (see details)
-#' @param \dots additional arguments passed to \code{max} (e.g. `na.rm = TRUE`)
 #'
 by_max <- function(subset, season = 1:12, ...) {
 
@@ -71,14 +62,10 @@ by_max <- function(subset, season = 1:12, ...) {
 #'
 #' @export
 #'
-#' @param subset a numeric vector defining years in which the metric should be calculated.
-#'   Can be a continuous or discontinuous set of years
-#' @param season define months of year in which to calculate the metric (see details)
 #' @param fun a function (unquoted) used to calculate the final metric. Defaults
 #'   to \code{median} but any R function that returns a single numeric value will
 #'   work. The \pkg{aae.data} provides three additional options: \code{days_below},
 #'   \code{days_above}, and \code{rolling_range}
-#' @param \dots additional arguments passed to \code{fun} (e.g. `na.rm = TRUE`)
 #'
 by_generic <- function(subset, season = 1:12, fun = median, ...) {
 
