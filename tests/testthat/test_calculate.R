6494daed315fdc20d5ccfa3c509c28ad7862ab03
Jian Yen <jdl.yen@gmail.com>
2020-05-20 13:29:37 +1000

    update fetch_data tests and remove redundant tests of rescale and resolution

--- tests/testthat/test_calculate.R
+++ tests/testthat/test_calculate.R
@@ -1,185 +1,108 @@
 context("calculate")
 
-test_that("calculate returns correct values without subsetting or lags", {
+# setup: load some packages
+library(dplyr)
+library(lubridate)
 
-  nyear <- years(5)
-  start <- dmy("01012010")
-  flow_sim <- data.frame(
-    date = seq(start, start + nyear, by = 1)
+# setup: simulate some data to test against
+nyear <- years(5)
+start <- dmy("01012010")
+flow_sim <- data.frame(
+  date = seq(start, start + nyear, by = 1)
+)
+flow_sim$month <- month(flow_sim$date)
+flow_sim$year <- year(flow_sim$date)
+flow_sim$value <- exp(rnorm(nrow(flow_sim)))
+
+# setup: define function to calculate averages and return
+#   appopriate types
+calculate_manual <- function(value, class, fun = "median", subset = NULL) {
+  out <- tapply(value, class, fun)
+  out <- data.frame(
+    date = as.numeric(names(out)),
+    metric = out
   )
-  flow_sim$value <- exp(rnorm(nrow(flow_sim)))
+  rownames(out) <- NULL
+  if (!is.null(subset)) {
+    out <- out[out$date %in% subset, ]
+  }
+  out
+}
+
+test_that("calculate returns correct survey values with different seasons", {
+
+  # all months and all years
+  value <- flow_sim %>% do(
+    calculate(
+      value = .$value,
+      date = .$date,
+      resolution = survey(season = 1:12, lag = 0),
+      na.rm = TRUE
+    )
+  )
+  target <- calculate_manual(flow_sim$value, flow_sim$year + 1, subset = 2010:2015)
+  expect_equal(value, target)
+
+  # each month separately
+  for (i in seq_len(24)) {
+    value <- flow_sim %>% do(
+      calculate(
+        value = .$value,
+        date = .$date,
+        resolution = survey(season = i, lag = 0),
+        na.rm = TRUE
+      )
+    )
+    if (i <= 12) {
+      flow_tmp <- flow_sim %>% filter(month == i)
+      flow_tmp$year <- flow_tmp$year + 1
+    } else {
+      flow_tmp <- flow_sim %>% filter(month == i)
+    }
+    target <- calculate_manual(flow_sim$value, flow_sim$year, subset = 2010:2015)
+    expect_equal(value, target)
+  }
 
 })
 
+test_that("calculate returns correct annual values with different seasons", {
 
-# # test calculate_metrics
-#
-# # check all resolutions with and without subsets
-# #  check annual, baseline, and survey with seasons
-# #  check lags
-# spring_flow <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = survey(season = 9:11, lag = 0, subset = 2010:2016),
-#   na.rm = TRUE
-# )
-#
-# test_lag <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = annual(season = 9:11, lag = 1, subset = 2010:2016),
-#   na.rm = TRUE
-# )
-# test_no_lag <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = annual(season = 9:11, lag = 0, subset = 2009:2015),
-#   na.rm = TRUE
-# )
-#
-# spring_flow_lt <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = survey(season = 9:11, lag = 0, subset = 2009:2015),
-#   rescale = list(subset = 2004:2020),
-#   na.rm = TRUE
-# )
-#
-# baseline_flow <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = baseline(subset = 2004:2020),
-#   na.rm = TRUE
-# )
-#
-# annual_ave <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = annual(subset = 2009:2015),
-#   na.rm = TRUE
-# )
-#
-# monthly_ave <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = monthly(subset = 2009:2015),
-#   na.rm = TRUE
-# )
-#
-# weekly_ave <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = weekly(subset = 2009:2015),
-#   na.rm = TRUE
-# )
-#
-# annual_max <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = annual(subset = 2009:2015),
-#   fun = max,
-#   na.rm = TRUE
-# )
-#
-# q10_est <- calculate(
-#   flow_405232$value_stream_discharge_mld,
-#   flow_405232$date_formatted,
-#   resolution = baseline(season = 5:7),
-#   fun = quantile,
-#   p = 0.1,
-#   na.rm = TRUE
-# )
-# low_flow_days <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = survey(),
-#   fun = days_below,
-#   threshold = q10_est$metric,
-#   na.rm = TRUE
-# )
-#
-# flow_var <- calculate(
-#   value = flow_405232$value_stream_discharge_mld,
-#   date = flow_405232$date_formatted,
-#   resolution = survey(season = 10:12, subset = 2007:2019),
-#   fun = rolling_range,
-#   lag = 3,
-#   na.rm = TRUE
-# )
-#
-#
-# # multiple sites
-# test_multi <- flow_data %>%
-#   group_by(site_name) %>%
-#   group_map(
-#     calculate,
-#     value = .$value_stream_discharge_mld,
-#     date = .$date_formatted,
-#     resolution = survey(season = 9:11),
-#     na.rm = TRUE
-#   ) %>%
-#   ungroup
-#
-# test_multi <- flow_data %>%
-#   group_by(site_name, site_code) %>%
-#   do(
-#     calculate(
-#       value = .$value_stream_discharge_mld,
-#       date = .$date_formatted,
-#       resolution = survey(season = 9:11),
-#       na.rm = TRUE
-#     )
-#   ) %>%
-#   ungroup
-#
-# test_multi <- flow_long %>%
-#   group_by(site_name, site_code, variable_name) %>%
-#   do(
-#     calculate(
-#       value = .$value,
-#       date = .$date_formatted,
-#       resolution = survey(season = 9:11),
-#       na.rm = TRUE
-#     )
-#   ) %>%
-#   ungroup
-#
-# # several options
-# test_multi_wide <- test_multi %>%
-#   pivot_wider(id_cols = date,
-#               names_from = c(variable_name, site_code),
-#               values_from = metric)
-#
-# test_multi_wide <- test_multi %>%
-#   pivot_wider(id_cols = c(site_name, site_code, date),
-#               names_from = variable_name,
-#               values_from = metric)
-#
-# test_multi_wide <- test_multi %>%
-#   pivot_wider(id_cols = date,
-#               names_from = c(variable_name, site_code),
-#               values_from = metric)
-#
-# test_multi <- flow_long %>%
-#   filter(variable_name == "stream_discharge_mld") %>%
-#   group_by(site_name, site_code) %>%
-#   do(
-#     calculate(
-#       value = .$value,
-#       date = .$date_formatted,
-#       resolution = survey(season = 9:11),
-#       na.rm = TRUE
-#     )
-#   ) %>%
-#   ungroup
-#
-#
-# # not working now but will work in dplyr v.1.0.0
-# flow_long %>%
-#   filter(variable_name == "stream_discharge_mld") %>%
-#   group_by(site_name) %>%
-#   summarise(
-#     spring_flow = calculate(value, date_formatted, survey(season = 9:11), na.rm = TRUE)$metric,
-#     summer_flow = calculate(value, date_formatted, survey(season = 12:3), na.rm = TRUE)$metric
-#   )
-#
+
+})
+
+test_that("calculate returns correct annual values with different functions", {
+
+  # don't forget counts of days above/below and rolling range
+
+  # plus standard meidna, mean, max, etc.
+})
+
+test_that("calculate returns correct baseline values with different seasons", {
+
+
+})
+
+test_that("calculate returns correct weekly values", {
+
+
+})
+
+test_that("calculate returns correct monthly values", {
+
+
+})
+
+test_that("calculate returns correct values with lags", {
+
+
+})
+
+test_that("calculate returns correct values with subsets", {
+
+
+})
+
+test_that("calculate returns correct values with rescaling", {
+
+
+})
--- tests/testthat/test_fetch_data.R
+++ tests/testthat/test_fetch_data.R
@@ -7,11 +7,8 @@ library(tidyr)
 
 # setup: load and clean some small data files to test downloads
 gauge_list <- c(225200, 405202, 406201, 409200)
-# data_list <- lapply(
-#   gauge_list, function(x) system.file("testdata", paste0(x, ".csv"), package = "aae.data")
-# )
 data_list <- lapply(
-  gauge_list, function(x) read.csv(paste0("inst/testdata/", x, ".csv"))
+  gauge_list, function(x) system.file("testdata", paste0(x, ".csv"), package = "aae.data")
 )
 data_list <- lapply(
   data_list,
@@ -42,9 +39,9 @@ test_that("fetch_data returns correct values", {
                   values_from = c("value", "quality_code"))
 
     # check equal for both variables
-    expect_equal(data_tmp$value_stream_water_level_m, data_list[[i]]$water_level_m)
+    expect_equal(data_tmp$value_stream_water_level_m, as.numeric(data_list[[i]]$water_level_m))
     expect_equal(data_tmp$quality_code_stream_water_level_m, data_list[[i]]$qc_water_level)
-    expect_equal(data_tmp$value_stream_discharge_mld, data_list[[i]]$discharge_mld)
+    expect_equal(data_tmp$value_stream_discharge_mld, as.numeric(data_list[[i]]$discharge_mld))
     expect_equal(data_tmp$quality_code_stream_discharge_mld, data_list[[i]]$qc_discharge)
 
   }
@@ -57,29 +54,105 @@ test_that("include_missing works correctly in fetch_data", {
   # checks gap filling when multiple sites have inconsistent data availability
   with_missing <- fetch_data(
     sites = gauge_list[1],
-    start = date_range[[1]][1], end = date_range[[1]][2],
+    start = "2017-09-29", end = "2017-10-02",
     variables = c("flow", "temp"),
     include_missing = TRUE
   )
   without_missing <- fetch_data(
     sites = gauge_list[1],
-    start = date_range[[1]][1], end = date_range[[1]][2],
+    start = "2017-09-29", end = "2017-10-02",
     variables = c("flow", "temp"),
     include_missing = FALSE
   )
+
+  # expect padded NAs in with_missing, not there in without_missing
+  expect_equal(nrow(with_missing), 2 * nrow(without_missing))
+
+  # expect only a single variable when !include_missing
+  expect_length(unique(without_missing$variable_code), 1L)
+  expect_length(unique(with_missing$variable_code), 2L)
+
+  # check that missing values are all NA
+  expect_equal(with_missing$value[with_missing$variable_code == "450.00"],
+               as.numeric(rep(NA, 4L)))
 
 })
 
 # check warnings and errors come up appropriately
 test_that("fetch_data messages, warns, and errors informatively", {
 
-  # check message for partial and fully missing data on download
+  # check message for fully missing data on download
+  expect_message(
+    fetch_data(
+      sites = gauge_list[1],
+      start = date_range[[1]][1], end = date_range[[1]][2],
+      variables = c("flow", "temp"),
+      include_missing = FALSE
+    ),
+    "No data for the following sites and variables:\n225200: temperature"
+  )
 
-  # check warnings and errors (none in fetch_data?)
+  # check message for partially missing data on download
+  expect_message(
+    fetch_data(
+      sites = gauge_list[1],
+      start = "1992-04-25", end = "1992-05-02",
+      variables = c("flow"),
+      include_missing = FALSE
+    ),
+    "Incomplete data for the following sites and variables:\n225200: discharge"
+  )
+
+  # check message for partial and fully missing data on download
+  expect_message(
+    fetch_data(
+      sites = gauge_list[1],
+      start = "1992-04-25", end = "1992-05-02",
+      variables = c("flow", "temp"),
+      include_missing = FALSE
+    ),
+    "No data for the following sites and variables:\n225200: temperature\n\nIncomplete data"
+  )
+
+  # check message for multiple sites
+  expect_message(
+    fetch_data(
+      sites = gauge_list[1:3],
+      start = "1975-06-11", end = "1975-06-13",
+      variables = c("flow"),
+      include_missing = FALSE
+    ),
+    "No data for the following sites and variables:\n225200:",
+    " temperature\n\nIncomplete data for the following sites and",
+    " variables:\n405202: discharge"
+  )
+
+  # check message for list_variables call
+  expect_message(
+    fetch_data(
+      sites = gauge_list[1:3],
+      start = "1975-06-11", end = "1975-06-13",
+      variables = c("flow"),
+      include_missing = FALSE
+    ),
+    "Use list_variables\\(c\\(225200, 405202\\)\\)"
+  )
 
 })
 
 # make sure check_quality catches all unique QC values
 test_that("check_quality returns all QC values", {
+
+  flow <- fetch_data(
+    sites = gauge_list[1],
+    start = date_range[[1]][1], end = date_range[[1]][2],
+    variables = c("flow", "temp"),
+    include_missing = FALSE
+  )
+
+  qc <- check_quality(flow)
+  value <- qc$quality_code %>% levels %>% as.numeric %>% sort
+  target <- flow$quality_code %>% unique %>% sort
+  expect_equal(value, target)
 
 })
